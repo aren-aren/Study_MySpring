79e5a27ff459e68c00157333dcc9b3c147acdc98
sangyeol9 <xhdfud1@naver.com>
2024-01-03 17:39:33 +0900

    20240103 di product

--- src/main/java/com/moveone/app/product/ProductDAO.java
+++ src/main/java/com/moveone/app/product/ProductDAO.java
@@ -1,15 +1,53 @@
 package com.moveone.app.product;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
+
+import com.studyservlet.app.util.DBConnector;
 
 public class ProductDAO {
 	
-	public List<ProductDTO> getList(){
-		return null;
+	public List<ProductDTO> getList() throws Exception{
+		Connection con = DBConnector.getConnector();
+		String sql = "SELECT * FROM PRODUCT";
+		PreparedStatement st = con.prepareStatement(sql);
+		ResultSet rs = st.executeQuery();
+		ProductDTO dto = new ProductDTO();
+		List<ProductDTO> ar = new ArrayList<ProductDTO>();
+		if(rs.next()) {
+			dto.setProductNum(rs.getLong("productNum"));
+			dto.setProductName(rs.getString("productName"));
+			dto.setProductContents(rs.getString("productNontents"));
+			dto.setProductRate(rs.getDouble("productRate"));
+			dto.setProductJumsu(rs.getDouble("productJumsu"));
+			ar.add(dto);
+		}
+		
+		DBConnector.disConnect(rs, st, con);
+		
+		return ar;
 	}
 	
-	public ProductDTO getDetail(ProductDTO productDTO) {
-		return null;
+	public ProductDTO getDetail(ProductDTO productDTO) throws Exception {
+		Connection con = DBConnector.getConnector();
+		String sql = "SELECT * FROM WHERE = PRODUCTNUM = ?";
+		PreparedStatement st = con.prepareStatement(sql);
+		st.setLong(1, productDTO.getProductNum());
+		ResultSet rs = st.executeQuery();
+		
+		productDTO.setProductContents(rs.getString("productContents"));
+		productDTO.setProductJumsu(rs.getDouble("productJumsu"));
+		productDTO.setProductName(rs.getString("productName"));
+		productDTO.setProductNum(rs.getLong("productNum"));
+		productDTO.setProductRate(rs.getDouble("productRate"));
+		
+		DBConnector.disConnect(rs, st, con);
+		
+		
+		return productDTO;
 	}
 	
 	public Integer addProduct(ProductDTO productDTO) {
