fa0c531a39040b2d0ddf98818f3dbe9f7c0a5ab3
dongil <az330015@gmail.com>
2024-01-03 10:42:20 +0900

    make temporary test class

---
+++ src/test/java/com/moveone/app/MyTest.java
@@ -0,0 +1,14 @@
+package com.moveone.app;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
